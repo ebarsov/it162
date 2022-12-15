e5ee34bb9318f5c9937c898194e82ff78ef82384
ebarsov <109013764+ebarsov@users.noreply.github.com>
2022-12-15 12:51:08 -0800

    Add files via upload

---
+++ final/template.php
@@ -0,0 +1,53 @@
+<?php include 'includes/header.php';?>
+         
+
+         <h2 class="welcome">Welcome</h2>
+
+<!------------------ START LEFT COL ----------------------------->
+         <div "Section + Aside container" class="group">
+            <section class="left-section">
+               <img src="images/restaurant.png" class="restaurant"  alt="Restaurant">
+               <div class="paragraph">
+                     <p>Welcome ~ Вітаємо to <strong>Pampushka</strong> Restaurant where dining is just the beginning of your journey. Step into a total cultural experience where great food is just a bonus. At <strong>Pampushka</strong> we take Ukrainian cuisine to a new level.</p>
+                     <p>The menu at <strong>Pampushka</strong> leaps beyond the old familiar basics such as borsch, cabbage rolls and pyrohy. Of course we serve all those traditional dishes you would expect to find on a Ukrainian menu. However, we also explore the more exotic dishes representing various regions of Ukraine. You will taste Ukrainian dishes you never dreamed existed. Our frequent travel to Ukraine brings authentic recipes from numerous fine restaurants in Ukraine from Kyiv to Lviv to Bukovyna to the Black Sea and more.</p>
+                     <p>To complement your dining adventure we serve imported beers, wines, fine champagnes and several premium vodka choices from Ukraine. Sample such Ukrainian liquor labels as Slavutich, Lvivske, Krimsekt, Odessa, Slava, Zirkova, Khortytsya, and Nemiroff to name but a few.</p>
+                     <p>At <strong>Pampushka</strong> you will certainly stimulate all your senses -- the Ukrainian way. When we say ‘taste’ we involve more than just your taste buds. Be prepared for sights, sounds, smells, tastes and feelings that are sure to transport you to another world and another time.</p>
+                     <p>A visit to <strong>Pampushka</strong> begins before you even cross the threshold. You will immediately be taken on a journey to a Ukrainian village as you approach what appears from the exterior to be a Ukrainian house with its thatched roof and woven willow fence.  As you enter you may choose your own adventure by dining in the setting of the courtyard of an 18th century village house or alongside a mural that takes you into a village market place bazar. Feeling romantic? Take a table under the romantic starlight of the orchard. Or surround yourself by original art from Ukraine that will inspire any appetite.</p>
+                     <p>Plenty to see—plenty to taste. The journey to Ukraine begins at <strong>Pampushka</strong>. Whether it’s a quiet dinner or a party of celebration we will be happy to serve you and we hope to have that opportunity soon. Bring your appetite and your celebration to us.</p>
+                     <p>We await your arrival! Ми вас радо чекаємо!</p>
+               </div>
+            </section>
+
+<!------------------------------------------------------ START RIGHT COL ----------------------------------------------------------------------->
+            <aside id="recent-posts-3" class="aside-news">
+		      <h3 class="news-events">News &#038; Events</h3>
+                  <nav aria-label="News &#038; Events">
+		               <ul>
+						      <li class="brake"> 
+					             <!--<a href="https://tasteofukraine.com/christmas-orders/">-->CHRISTMAS ORDERS<!--</a>-->
+							   </li>
+							   <li class="brake">
+					            <!--<a href="https://tasteofukraine.com/perogies-in-the-parking-lot/">-->&#8220;PEROGIES IN THE PARKING LOT&#8221; Fund Raising Event<!--</a>-->
+							   </li>
+							   <li class="brake">
+					            <!--<a href="https://tasteofukraine.com/russian-invasion-of-ukraine/">-->RUSSIAN INVASION OF UKRAINE<!--</a>-->
+							   </li>
+							   <li class="brake">
+					            <!--<a href="https://tasteofukraine.com/covid-19-update/">-->COVID-19 Update<!--</a>-->
+							   </li>
+							   
+					      </ul> 
+		            </nav>
+            </aside>&nbsp;
+      
+            <aside id="text-6" class="aside-comments">	
+               <h3 class="review">Review</h3>		
+               <p>
+                   Add your comments about Pampushka Restaurant
+               </p>
+               <a href="/add-a-restaraunt-review/">Click Here</a>
+            </aside>
+         </div>
+   <!-----------------------END RIGHT COL --------------------->
+
+   <?php include 'includes/footer.php';?>
